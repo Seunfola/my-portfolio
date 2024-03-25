d3f0cccf9c3d479ee1dc91346f0c1bbf0739734a
Folahan O Mosunmola <seunfola1@gmail.com>
2024-03-25 20:28:01 +0100

    Update data.js

--- src/app/info/[category]/data.js
+++ src/app/info/[category]/data.js
@@ -1,36 +1,36 @@
 export const items = {
 
     LearnlyDashboard: [
-        {
+        
             id: 1,
             title: "Task Note",
             desc: "It a web application that register tasks, set Alarm, play the alarm tune and render the tasklist, has filter and search and pagination, Employed redux for state management",
             image: "/learnly.jpg",
             url: "https://task-dashboard-r34k.vercel.app/",
             repo:"https://github.com/Seunfola/task-dashboard",
-        },
+        
 
     ],
     OnlineStore: [
-        {
+        
             id: 1,
             title: "Breed Store",
             desc: "Breed Store is a web application that displays products, carts, homepage and checkout page. Used Redux to manage the state, used react-hot-toast to set the notification, employed reuseable component and used firebase for login authentication",
             image: "/BreedStore.png",
             url: "https://bulb-capstone-9b12.vercel.app/home",
             repo:"https://github.com/Seunfola/Bulb-Capstone/tree/main",
-        },
+        
 
     ],
     BreedRealEstate: [
-        {
+        
             id: 1,
             title: "Breed Real Estate",
             desc: "Breed Real Estate that displays the case for properties for sales for potential clients It is a static website and it was built using vite, it is completely mobile response",
             image: "/BreedHm.png",
             url: "https://real-estate-lemon-five.vercel.app/",
             repo:"https://github.com/Seunfola/Real-Estate",
-        },
+        
 
     ]
 };
